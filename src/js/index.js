e4816322737d8e6052b74de9033a49fe99d86c78
Raul Andrial <rauljandrial@gmail.com>
2022-10-26 19:32:42 +0000

    vercel changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,23 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+//font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faClock);
+
 //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let i = 0;
+function intervalRender() {
+
+    i++;
+    let numArr = String(i).padStart(6, "0").split('');
+    ReactDOM.render(<Home numArr={numArr}/>, document.querySelector("#app"));
+    if (i === 999999) clearInterval(intervalID);
+}
+
+const intervalID = setInterval(intervalRender, 1000);
