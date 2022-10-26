a7b2a8d4d46ba7552dcb28f46d5d0fc396cd4473
Raul Andrial <rauljandrial@gmail.com>
2022-10-26 04:46:07 +0000

    Refactored some code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,17 +13,17 @@ import { faClock } from "@fortawesome/free-regular-svg-icons";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = ({a, b, c, d, e, f}) => {
+const Home = ({numArr}) => {
 	return (
 		<div className="container-fluid ">
 			<div className="row d-flex justify-content-center mt-5 p-5">
-				<FACard innerValue={<FontAwesomeIcon icon={faClock} />} style={{width: "200px !important"}}/>
-				<NumCard innerValue={f}/>
-				<NumCard innerValue={e}/>
-				<NumCard innerValue={d}/>
-				<NumCard innerValue={c}/>
-				<NumCard innerValue={b}/>
-				<NumCard innerValue={a}/>
+				<FACard innerValue={<FontAwesomeIcon icon={faClock} />} />
+				<NumCard innerValue={numArr[0]}/>
+				<NumCard innerValue={numArr[1]}/>
+				<NumCard innerValue={numArr[2]}/>
+				<NumCard innerValue={numArr[3]}/>
+				<NumCard innerValue={numArr[4]}/>
+				<NumCard innerValue={numArr[5]}/>
 			</div>
 		</div>
 	);
